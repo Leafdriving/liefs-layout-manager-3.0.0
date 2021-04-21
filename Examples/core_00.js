1ea7bd341430974bb6b086cac5371bb1b8712370
electriclief <electriclief@gmail.com>
2021-04-21 17:18:52 -0400

    xxx

---
+++ Examples/core_00.js
@@ -0,0 +1,8 @@
+let greenBGCss = css("greenBG", "background:green");
+let blueBGCss = css("blueBG", "background:blue;color:white");
+H("core_00", // create a "Handler" to handle screen size change
+    h(`core_00_h`,  // create a horizontal DisplayGroup to hold an array of DisplayCells
+        I("core_00_left","Left Side", greenBGCss), // Create an Element (div)
+        I("core_00_right","Right Side", blueBGCss), // Create an Element (div)
+    )
+);
--- Examples/core_01.js
+++ Examples/core_01.js
@@ -2,7 +2,9 @@ let greenBGCss = css("greenBG", "background:green");
 let blueBGCss = css("blueBG", "background:blue;color:white");
 let redBGCss = css("redBG", "background:red");
 H("core_01", 10, // set 10 pixels border on outside
-h(`core_01_h`, -5, // set -5 pixels between cells (to offset below)
-I("core_01_left", "Left Side", greenBGCss, 5), // 5 pixel margin all sides
-I("core_01_right", "Right Side", blueBGCss, 5)), I(`core_01_background`, redBGCss) // Background 
+    h(`core_01_h`, -5, // set -5 pixels between cells (to offset below 5+5-5= 5 pixels)
+        I("core_01_left","Left Side", greenBGCss, 5), // 5 pixel margin all sides
+        I("core_01_right","Right Side", blueBGCss, 5), // 5 pixel margin all sides
+    ),
+    I(`core_01_background`, redBGCss) // Background 
 );
